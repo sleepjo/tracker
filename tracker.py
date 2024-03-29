35449e0ce559d01d41bf4aadf691fc6069de404c
kenryu <jryu1521@gmail.com>
2024-03-29 12:52:01 +0900

    tracking system done

---
+++ tracker.py
@@ -0,0 +1,29 @@
+import json
+
+following_path = 'insta-unfollow-tracker/following.json'
+follower_path = 'insta-unfollow-tracker/followers.json';
+with open(following_path, 'r') as fp:
+    data = json.load(fp)
+
+# check the following list first
+tracklist = []
+for relationship in data['relationships_following']:
+        for profile_info in relationship['string_list_data']:
+            tracklist.append(json.dumps(profile_info['value']))
+
+with open(follower_path, 'r') as fp:
+    data = json.load(fp)
+
+for item in data:
+    for profile_info in item['string_list_data']:
+        if json.dumps(profile_info['value']) in tracklist:
+             tracklist.remove(json.dumps(profile_info['value']));
+        # print(json.dumps(profile_info['value'], indent=4));
+file_name = "track.txt"
+with open(file_name, 'w') as file:
+    for data in tracklist:
+        file.write(data + '\n')
+
+print('done')
+
+
